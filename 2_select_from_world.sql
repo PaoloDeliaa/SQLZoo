7686e1b0041a36f19854bc7255db6f9ba574f544
PaoloDeliaa <paolo.delia@studenti.isissgobetti.it>
2024-10-22 13:49:19 +0200

    yee

--- 2_select_from_world.sql
+++ 2_select_from_world.sql
@@ -1,35 +1,36 @@
 -- # 1. Read the notes about this table. Observe the result of running
 -- # a simple SQL command.
-SELECT name, continent, population
-FROM world;
+
 
 -- # 2. Show the name for the countries that have a population of at
 -- # least 200 million. (200 million is 200000000, there are eight
 -- # zeros)
-SELECT name
-FROM world
-WHERE population > 200000000;
+SELECT NAME
+FROM world.world
+where population > 200000000;
 
 -- # 3. Give the name and the per capita GDP for those countries with a
 -- # population of at least 200 million.
 SELECT name, gdp/population
-FROM world
+FROM world.world
 WHERE population > 200000000;
+
 
 -- # 4. Show the name and population in millions for the countries of
 -- # 'South America' Divide the population by 1000000 to get population
 -- # in millions.
-SELECT name, population/1000000
-FROM world
+SELECT ame, population/1000000
+FROM world.world
 WHERE continent = 'South America';
 
+
 -- # 5. Show the name and population for 'France', 'Germany', 'Italy'
-SELECT name, population
-FROM world
-WHERE name IN ('France', 'Germany', 'Italy');
+SELCT name, population
+FROM world.world
+where name in ('FRANCE', 'GERMANY', 'ITALY');
 
 -- # 6. Identify the countries which have names including the word
 -- # 'United'
-SELECT name
-FROM world
-WHERE name LIKE 'United%';
+SELECT NAME
+FROM world.world
+WHERE name like '%United%';
